68d482e31c2553c24874e19c74895d31519bdaea
cplunk <cplunk@mydotcomdotnet.org>
2015-04-16 19:40:16 -0700

    summary

--- learning_journal/learning_journal/views.py
+++ learning_journal/learning_journal/views.py
@@ -9,13 +9,30 @@ from .models import (
     )
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'learning_journal'}
+##@view_config(route_name='home', renderer='templates/mytemplate.pt')
+##def my_view(request):
+##    try:
+##        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
+##    except DBAPIError:
+##        return Response(conn_err_msg, content_type='text/plain', status_int=500)
+##    return {'one': one, 'project': 'learning_journal'}
+##
+
+@view_config(route_name='home', renderer='string')
+def index_page(request):
+    return {entry.all, 'list page'}
+
+@view_config(route_name='detail', renderer='string')
+def view(request):
+    return 'detail page'
+
+@view_config(route_name='action', match_param='action=create', renderer='string')
+def create(request):
+    return 'create page'
+
+@view_config(route_name='action', match_param='action=edit', renderer='string')
+def update(request):
+    return 'edit page'
 
 
 conn_err_msg = """\
